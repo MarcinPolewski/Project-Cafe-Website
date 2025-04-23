08875db2695c3287da6ada49feafce8fa7052b57
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-04-23 21:18:07 +0200

    hot fix to achieve compliance with linter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LanguageSwitcher from './components/LanguageSwitcher'
 import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return <>
     <h1>{t('test')}</h1>
