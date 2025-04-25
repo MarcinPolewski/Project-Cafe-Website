81847b734756982be3fe5373a3c80177e8269e74
MarcinPolewski <30431016+MarcinPolewski@users.noreply.github.com>
2025-04-25 19:37:06 +0200

    Add routing (#5)
    * added dependency
    
    * added basic routing
    
    * fixed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import './App.css'
-import LanguageSwitcher from './components/LanguageSwitcher'
-import { useTranslation } from 'react-i18next';
+// import LanguageSwitcher from './components/LanguageSwitcher'
+// import { useTranslation } from 'react-i18next';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import RootLayout from './layouts/RootLayout';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'about', element: <AboutPage /> },
+      { path: 'contact', element: <ContactPage /> },
+    ]
+  }]);
+
 
 function App() {
-  const { t } = useTranslation();
+  return (
+    <RouterProvider router={router} />
+  )
+  // const { t } = useTranslation();
 
-  return <>
-    <h1>{t('test')}</h1>
-    <LanguageSwitcher />
-  </>
+  // return <>
+  //   <h1>{t('test')}</h1>
+  //   <LanguageSwitcher />
+  // </>
 }
 
 export default App;
