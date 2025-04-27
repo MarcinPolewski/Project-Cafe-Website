f77a780c708092e127ac79dd94e8ce92fcc5a518
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-04-27 12:20:48 +0200

    fixed issues with highlighting selected link

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -16,21 +16,21 @@ function NavigationBar() {
                 <Logo />
                 <NavLink
                     to="/"
-                    className={({ isActive }) => isActive ? classes.activeNavLink : classes.navLink}
+                    className={({ isActive }) => isActive ? "active" : ""}
                     end
                 >
                     {t("nav.home")}
                 </NavLink>
                 <NavLink
                     to="/about"
-                    className={({ isActive }) => isActive ? classes.activeNavLink : classes.navLink}
+                    className={({ isActive }) => isActive ? "active" : ""}
                     end
                 >
                     {t("nav.about")}
                 </NavLink>
                 <NavLink
                     to="/contact"
-                    className={({ isActive }) => isActive ? classes.activeNavLink : classes.navLink}
+                    className={({ isActive }) => isActive ? "active" : ""}
                     end
                 >
                     {t("nav.contact")}
