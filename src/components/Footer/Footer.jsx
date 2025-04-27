8053af2bb51e87baeeb546f63ae1496cf680b241
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-04-27 09:49:35 +0200

    implemented i18n to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import classes from './Footer.module.css';
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+
+    const { t } = useTranslation();
 
     return (
         <div className={classes.footer}>
             <span>©2025 marcin.jacek.polewski@gmail.com </span>
-            <a>Privacy Policy</a>
-            <a>Terms of Service</a>
+            <a>{t('footer.privacyPolicy')}</a>
+            <a>{t('footer.termsOfService')}</a>
         </div>);
 }
 
