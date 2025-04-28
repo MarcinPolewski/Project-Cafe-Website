f3851fbe992d31333b2a1e500101b8db8e9c0ca5
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-04-28 09:34:59 +0200

    added translation to polish, modified css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,9 @@
+import { useTranslation } from 'react-i18next';
 import classes from './ContactForm.module.css';
 
 function ContactForm({ onSubmit, heading, ...props }) {
+    const { t } = useTranslation();
+
     function handleSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
@@ -14,20 +17,20 @@ function ContactForm({ onSubmit, heading, ...props }) {
             <h1>{heading}</h1>
             <form onSubmit={handleSubmit}>
                 <div className={classes.inputWithHeading}>
-                    <label htmlFor="name">Name:</label>
+                    <label htmlFor="name">{t("contact.form.name")}:</label>
                     <input type="text" id="name" name="name" required />
                 </div>
                 <div className={classes.inputWithHeading}>
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email">{t("contact.form.email")}:</label>
                     <input type="email" id="email" name="email" required />
                 </div>
                 <div className={classes.textAreaWithHeading}>
-                    <label htmlFor="message">Message:</label>
+                    <label htmlFor="message">{t("contact.form.message")}:</label>
                     <textarea id="message" name="message" required></textarea>
                 </div>
-                <div>
-                    <button type="submit">Send</button>
-                    <button type="reset">Reset</button>
+                <div className={classes.buttonContainer}>
+                    <button type="submit">{t("contact.form.submit")}</button>
+                    <button type="reset">{t("contact.form.reset")}</button>
                 </div>
             </form>
         </div>
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,21 +1,35 @@
+import { useTranslation } from "react-i18next";
 import ContactForm from "../components/ContactForm/ContactForm";
 import Panel from "../components/Panel/Panel";
 import { customerSentMessage } from "../util/serverComunication";
+import TitleValuePair from "../components/TitleValuePair/TitleValuePair";
 
-import classes from "./ContactPage.module.css";
+import _ from "./ContactPage.module.css";
+
+function TranslatedContactInformation({ type, t }) {
+
+    return (<TitleValuePair
+        title={t(`contact.${type}.title`)}
+        value={t(`contact.${type}.content`)}
+    />);
+}
 
 function ContactPage() {
+    const { t } = useTranslation();
+
     return (
         <Panel id="contactPanel1">
             <ContactForm
                 id="contactForm"
-                heading="let's talks"
+                heading={t("contact.formTitle")}
                 onSubmit={customerSentMessage}
             />
             <div id="contactInfo">
-                <h1>Contact us</h1>
-                <p>Phone: +48 123 456 789</p>
-                <p>Email:</p>
+                <h1>{t("contact.informationTitle")}</h1>
+                <TranslatedContactInformation type="email" t={t} />
+                <TranslatedContactInformation type="phone" t={t} />
+                <TranslatedContactInformation type="nip" t={t} />
+                <TranslatedContactInformation type="krs" t={t} />
             </div>
         </Panel>
 
