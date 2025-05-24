67bd1d933d7068dbc13164b4e313aa112ce8cb39
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-05-24 10:56:30 +0200

    added input field limit as first layer of defence against buffer overflow

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -20,15 +20,15 @@ function Form({ t, handleSubmit, heading }) {
             <form onSubmit={handleSubmit}>
                 <div className={classes.inputWithHeading}>
                     <label htmlFor="name">{t("contact.form.name")}:</label>
-                    <input type="text" id="name" name="name" required />
+                    <input type="text" id="name" name="name" required maxLength={50} />
                 </div>
                 <div className={classes.inputWithHeading}>
                     <label htmlFor="email">{t("contact.form.email")}:</label>
-                    <input type="email" id="email" name="email" required />
+                    <input type="email" id="email" name="email" required maxLength={50} />
                 </div>
                 <div className={classes.textAreaWithHeading}>
                     <label htmlFor="message">{t("contact.form.message")}:</label>
-                    <textarea id="message" name="message" required></textarea>
+                    <textarea id="message" name="message" required maxLength={500} minLength={5}></textarea>
                 </div>
                 <div className={classes.buttonContainer}>
                     <button type="submit">{t("contact.form.submit")}</button>
